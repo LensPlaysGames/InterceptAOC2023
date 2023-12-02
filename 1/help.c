1bad0dd804f0d79800cde2fdf399f8f857f4ef64
Lens_r <lensplaysgames@gmail.com>
2023-12-01 18:18:27 -0800

    [Day 1]

---
+++ 1/help.c
@@ -0,0 +1,25 @@
+/*
+#include <stdlib.h>
+
+size_t udiv(size_t lhs, size_t rhs) {
+  return lhs / rhs;
+}
+size_t umod(size_t lhs, size_t rhs) {
+  return lhs % rhs;
+}
+
+long long sdiv(long long lhs, long long rhs) {
+  return lhs / rhs;
+}
+long long smod(long long lhs, long long rhs) {
+  return lhs % rhs;
+}
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_number(size_t number) {
+  printf("%zu\n", number);
+}
